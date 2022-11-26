70d2e59d81da26c9ce60e24849517b09f6d0cfb6
Ammar Ajmal <ammarajml@gmail.com>
2022-11-26 23:55:27 +0900

    first commit

---
+++ build/CMakeFiles/cam_gige.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cam_gige.dir/src/main.cpp.o"
+  "cam_gige"
+  "cam_gige.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cam_gige.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
